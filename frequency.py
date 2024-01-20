cae28e9fe4c6031f43ca7c91c0e38b43b6a5a4ec
milkyway2404 <157184690+milkyway2404@users.noreply.github.com>
2024-01-20 10:12:44 -0700

    Add files via upload

---
+++ frequency.py
@@ -0,0 +1,9 @@
+#Write a program to compute the frequency of the words from the input.
+#The output should output after sorting the key alphanumerically.
+#○ Eg - line = “which is better python 2 or python 3”
+
+st = input().split()
+string = sorted(set(st))     # split text then store and sort as a set
+
+for i in string:
+    print("{0}:{1}".format(i,st.count(i)))
