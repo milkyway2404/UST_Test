392cb8649e227d3889b5f0e6ea9ab863a3ab85ec
milkyway2404 <157184690+milkyway2404@users.noreply.github.com>
2024-01-21 08:50:10 -0700

    Add files via upload
    Program 3 for validation of a password. The user will input multiple passwords and the program will report what passwords are valid/invalid based on the criteria of the task

---
+++ passwd.py
@@ -0,0 +1,29 @@
+l, u, p, d = 0, 0, 0, 0
+
+#s = "R@m@_f0rtu9e$"
+
+password_list = [str(password_list) for password_list in input("Enter multiple passwords: ").split(",")]
+print("List of passwords is: ", password_list)
+for s in password_list:
+    capitalalphabets="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    smallalphabets="abcdefghijklmnopqrstuvwxyz"
+    specialchar="$#@"
+    digits="0123456789"
+    if (len(s) >= 12):
+        for i in s:
+            # counting lowercase alphabets
+            if i in smallalphabets:
+                l += 1
+            # counting uppercase alphabets
+            if i in capitalalphabets:
+                u += 1
+            # counting digits
+            if i in digits:
+                d += 1
+            # counting the mentioned special characters
+            if i in specialchar:
+                p += 1
+    if l >= 1 and u >= 1 and p >= 1 and d >= 1 and l+p+u+d == len(s):
+        print(s, " Is Valid Password")
+    else:
+        print(s, " Is Invalid Password")
