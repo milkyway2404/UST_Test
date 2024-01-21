ce2af146b4027f5b380585c4c4df712a9a507386
milkyway2404 <157184690+milkyway2404@users.noreply.github.com>
2024-01-21 09:38:42 -0700

    Add files via upload
    updated programs to run from a main_func.py file

--- frequency.py
+++ frequency.py
@@ -1,9 +1,10 @@
 #Write a program to compute the frequency of the words from the input.
 #The output should output after sorting the key alphanumerically.
 #○ Eg - line = “which is better python 2 or python 3”
+def program1():
+    st = input().split()
+    string = sorted(set(st))     # split text then store and sort as a set
 
-st = input().split()
-string = sorted(set(st))     # split text then store and sort as a set
+    for i in string:
+        print("{0}:{1}".format(i,st.count(i)))
 
-for i in string:
-    print("{0}:{1}".format(i,st.count(i)))
---
+++ main_func.py
@@ -0,0 +1,9 @@
+from frequency import *
+from circ_queue import *
+from passwd import *
+
+#calling functions
+program1()
+program2()
+program3()
+
